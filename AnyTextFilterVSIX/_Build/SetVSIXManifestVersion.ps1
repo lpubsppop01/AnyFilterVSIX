d0c7c29d029bd2babd84cd7b7cebe4187caed391
lpubsppop01 <lpubsppop01@gmail.com>
2017-04-22 20:52:22 +0900

    change vsixmanifest versioning auto-increment on publish to copy AssemblyInfo

---
+++ AnyTextFilterVSIX/_Build/SetVSIXManifestVersion.ps1
@@ -0,0 +1,15 @@
+﻿$scriptDirPath = Split-Path $MyInvocation.MyCommand.Path -Parent
+$assemblyInfoPath = Join-Path $scriptDirPath "..\Properties\AssemblyInfo.cs"
+$assemblyVersionLine = Get-Content $assemblyInfoPath | Select-String AssemblyVersion | Select-Object -First 1
+$assemblyVersion = $assemblyVersionLine -replace '.*AssemblyVersion\("(.*)"\).*', '$1'
+$manifestFilePath = Join-Path $scriptDirPath "..\source.extension.vsixmanifest"
+$manifestXML = [xml](Get-Content $manifestFilePath)
+$identityElement = $manifestXML.SelectSingleNode("//*[local-name(.) = 'Identity']")
+
+#$oldVersion = $identityElement.Version
+#$v = [version]$identityElement.Version
+#$newVersion = $v.Major.ToString() + "." + $v.Minor.ToString() + "." + ($v.Build + 1).ToString()
+#Write-Host "Version: $oldVersion -> $newVersion"
+
+$identityElement.Version = $assemblyVersion
+$manifestXML.Save($manifestFilePath)
