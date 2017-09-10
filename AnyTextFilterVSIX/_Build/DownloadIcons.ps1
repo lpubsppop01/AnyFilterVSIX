fd461470207b315dd6fd058c91968b6fa1341ef9
lpubsppop01 <lpubsppop01@gmail.com>
2017-09-10 15:02:14 +0900

    Fix DownloadIcons.ps1

--- AnyTextFilterVSIX/_Build/DownloadIcons.ps1
+++ AnyTextFilterVSIX/_Build/DownloadIcons.ps1
@@ -2,6 +2,10 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Source
 $iconDirPath = Join-Path (Split-Path -Parent $here) "_Icons"
 $uriPrefix = "https://raw.githubusercontent.com/Templarian/WindowsIcons/master/WindowsPhone/xaml/"
 
+if (!(Test-Path -LiteralPath $iconDirPath)) {
+    mkdir $iconDirPath
+}
+
 function Download-XamlIcon($filename) {
     $filePath = (Join-Path $iconDirPath $filename)
     if (Test-Path -LiteralPath $filePath) {
